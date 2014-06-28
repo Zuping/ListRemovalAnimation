a9de34e17a9dac8ba6d7ac4e9c50c408de8d69f4
Zuping <lzppro@gmail.com>
2014-06-28 10:42:37 -0500

    10:43

--- app/src/main/java/com/android/zuping/listremovalanimation/ListActivity.java
+++ app/src/main/java/com/android/zuping/listremovalanimation/ListActivity.java
@@ -1,17 +1,54 @@
 package com.android.zuping.listremovalanimation;
 
 import android.app.Activity;
+import android.content.ReceiverCallNotAllowedException;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class ListActivity extends Activity {
+public class ListActivity extends Activity implements View.OnTouchListener {
+
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
+
+    private int mSlop;
+    private int mMinFlingVelocity;
+    private int mMaxFlingVelocity;
+    private long mAnimationTime;
+
+    private int mViewWidth;
+    private View mDownView;
+    private float mDownX;
+    private float mDownY;
+    private boolean mSwiping;
+    private int mSwipingSlop;
+
+    private VelocityTracker mVelocityTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        mListView = (ListView) this.findViewById(R.id.listView);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Cheeses.sCheeseStrings);
+        mListView.setOnTouchListener(this);
+
+        ViewConfiguration vc = ViewConfiguration.get(this);
+        mSlop = vc.getScaledTouchSlop();
+        mMinFlingVelocity = vc.getScaledMinimumFlingVelocity() * 16;
+        mMaxFlingVelocity = vc.getScaledMaximumFlingVelocity();
+        mAnimationTime = mListView.getContext().getResources().getInteger(
+                android.R.integer.config_shortAnimTime);
+        mViewWidth = mListView.getWidth();
     }
 
 
@@ -33,4 +70,44 @@ public class ListActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                Rect rect = new Rect();
+                int childCnt = mListView.getChildCount();
+                int[] listViewCords = new int[2];
+                mListView.getLocationOnScreen(listViewCords);
+                int x = (int) motionEvent.getRawX() - listViewCords[0];
+                int y = (int) motionEvent.getRawY() - listViewCords[1];
+                View child;
+                for (int i = 0; i < childCnt; i++) {
+                    child = mListView.getChildAt(i);
+                    child.getHitRect(rect);
+                    if (rect.contains(x, y)) {
+                        mDownView = child;
+                        break;
+                    }
+                }
+                if (mDownView != null) {
+                    mDownX = motionEvent.getRawX();
+                    mDownY = motionEvent.getRawY();
+
+                    mVelocityTracker = VelocityTracker.obtain();
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+
+    private class UndoCell {}
 }
